92104c66a0ca36acc19e947cb96fc0817d4ae7bc
Rio Caesar <ceriabersama@gmail.com>
2017-01-30 02:10:50 +0700

    Create helloWorld-2.cpp

---
+++ helloWorld-2.cpp
@@ -0,0 +1,17 @@
+/*
+  hello world using C++, version 2
+  ceriabersama@gmail.com
+  01/30/2017
+*/
+
+#include <iostream> // library that used
+
+using namespace std; // program using every std
+
+// function main begin
+int main()
+{
+    cout << "Hello World\n"; // print hello world command
+
+    return 0; // search in google for this habbits
+}
